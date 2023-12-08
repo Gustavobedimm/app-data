1e47acacbcaa6c5c2c9852607715fb6c4c92e74d
gustavobedimm <gustavo_bmazutti@hotmail.com>
2023-12-08 17:32:43 -0300

    correcao

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 
 function App() {
   const [texto, setTexto] = useState("");
+  const [texto2, setTexto2] = useState("");
+  const [texto3, setTexto3] = useState("");
 
   useEffect(() => {
     function calcula() {
@@ -17,7 +19,10 @@ function App() {
       const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
       var mesesTotal = (dataAtual.getFullYear() - dataNascimento.getFullYear())*12 + (dataAtual.getMonth() - dataNascimento.getMonth());
       const mesesRestantes = mesesTotal - (idade * 12);
-      setTexto("Estamos namorando a "+idade+" anos e "+mesesRestantes+ " meses ❤️❤️")
+      setTexto(idade+" anos e "+mesesRestantes+ " meses te amando ❤️❤️")
+      setTexto2("ou " + mesesTotal + "meses")
+      setTexto3("ou " + diffInDays + "dias")
+
     }
     calcula();
   }, []);
@@ -26,6 +31,12 @@ function App() {
       <header className="App-header">
         <p>
           {texto}
+        </p>
+        <p>
+          {texto2}
+        </p>
+        <p>
+          {texto3}
         </p>
         <img className="App-logo" src={img1} />
       </header>
